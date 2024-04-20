bc8ca2110a982d8ade934752be6350ceb29449f4
MASAMI Mashino <mashi.zzz@gmail.com>
2024-04-21 05:50:09 +0900

    make new def get_screeninfo

--- small_parts/get_graphics_info.py
+++ small_parts/get_graphics_info.py
@@ -1,13 +1,20 @@
 import platform
 
+
+
 osname = platform.system()
-if osname == 'Darwin':
-    from screeninfo import get_monitors
-    for m in get_monitors():
-        print(str(m))
-    #self.listWData.setFont(font)
-elif osname == 'Windows':
-    import ctypes
-    user32 = ctypes.windll.user32
-    screensize = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
-    print(screensize[0],screensize[1])
+
+def get_screensize():
+    if osname == 'Darwin':
+        from screeninfo import get_monitors
+        for monitor in get_monitors():
+            width = monitor.width
+            height = monitor.height
+        return width, height
+    elif osname == 'Windows':
+        import ctypes
+        user32 = ctypes.windll.user32
+        screensize = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
+        return screensize[0], screensize[1])
+
+print(get_screensize())
