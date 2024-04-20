942093441e718f2cbd0499010735bc5824e8f7e8
MASAMI Mashino <mashi.zzz@gmail.com>
2024-04-21 05:18:58 +0900

    mod print size separately

--- small_parts/get_graphics_info.py
+++ small_parts/get_graphics_info.py
@@ -10,4 +10,4 @@ elif osname == 'Windows':
     import ctypes
     user32 = ctypes.windll.user32
     screensize = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
-    print(screensize)
+    print(screensize[0],screensize[1])
