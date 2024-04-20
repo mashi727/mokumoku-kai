339cdda5a175b828b01341a15fd4234f37172d36
MASAMI Mashino <mashi.zzz@gmail.com>
2024-04-21 05:13:55 +0900

    add discri win or mac

--- small_parts/get_graphics_info.py
+++ small_parts/get_graphics_info.py
@@ -1,11 +1,12 @@
-import pyautogui as pag
+import platform
 
-print(pag.size())
-
-
-
-from screeninfo import get_monitors
-
-for m in get_monitors():
-
-    print(str(m))
+osname = platform.system()
+if osname == 'Darwin':
+    from screeninfo import get_monitors
+    for m in get_monitors():
+        print(str(m))
+    #self.listWData.setFont(font)
+elif osname == 'Windows':
+    import ctypes
+    user32 = ctypes.windll.user32
+    screensize = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
