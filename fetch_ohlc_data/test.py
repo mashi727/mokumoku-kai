6b7eb280e1cdf7eca531d0fb7194554d67f2f702
MASAMI Mashino <mashi.zzz@gmail.com>
2024-04-24 06:46:25 +0900

    update fetch code

---
+++ fetch_ohlc_data/test.py
@@ -0,0 +1,8 @@
+import requests
+
+# replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+url = 'https://alphavantageapi.co/timeseries/analytics?SYMBOLS=AAPL,MSFT,IBM&RANGE=2023-07-01&RANGE=2023-08-31&INTERVAL=DAILY&OHLC=close&CALCULATIONS=MEAN,STDDEV,CORRELATION&apikey=demo'
+r = requests.get(url)
+data = r.json()
+
+print(data)
