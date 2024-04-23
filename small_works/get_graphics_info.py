8a196ead64adf0d42956e33a14d70de116c3ffce
MASAMI Mashino <mashi.zzz@gmail.com>
2024-04-24 06:47:27 +0900

    rename small_parts to small_works

---
+++ small_works/get_graphics_info.py
@@ -0,0 +1,20 @@
+import platform
+
+
+
+osname = platform.system()
+
+def get_screensize():
+    if osname == 'Darwin':
+        from screeninfo import get_monitors
+        for monitor in get_monitors():
+            width = monitor.width
+            height = monitor.height
+        return width, height
+    elif osname == 'Windows':
+        import ctypes
+        user32 = ctypes.windll.user32
+        screensize = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
+        return screensize[0], screensize[1]
+
+print(get_screensize())
